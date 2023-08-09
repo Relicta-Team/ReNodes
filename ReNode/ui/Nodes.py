6b6e743afd73e6709bce4ff96bccb85383a8d508
Yodes <35444748+Yobas@users.noreply.github.com>
2023-08-09 19:20:12 +0300

    test node added

---
+++ ReNode/ui/Nodes.py
@@ -0,0 +1,17 @@
+import os
+import random
+from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets, QtCore, QtGui
+from NodeGraphQt import NodeGraph, BaseNode
+
+
+class RuntimeNode(BaseNode):
+
+	# unique node identifier domain.
+	__identifier__ = 'runtime_domain'
+
+	# initial default node name.
+	NODE_NAME = 'Default name'
+
+	def __init__(self):
+		super(RuntimeNode, self).__init__()
