c70bec9b09ac54e461dc2ea9c264fc84ce612e21
Yodes <35444748+Yobas@users.noreply.github.com>
2023-08-11 20:47:58 +0300

    Extended app logic (tests)

--- ReNode/ui/NodeGraphComponent.py
+++ ReNode/ui/NodeGraphComponent.py
@@ -4,7 +4,8 @@ from PyQt5.QtCore import *
 from PyQt5 import QtCore, QtGui
 from PyQt5 import *
 
-from NodeGraphQt import (NodeGraph, GroupNode)
+from NodeGraphQt import (NodeGraph, GroupNode, NodeGraphMenu)
+from NodeGraphQt.base.menu import NodesMenu
 from NodeGraphQt.custom_widgets.properties_bin.node_property_widgets import PropertiesBinWidget
 from NodeGraphQt.qgraphics.node_base import NodeItem
 from NodeGraphQt.widgets.viewer import NodeViewer
@@ -17,6 +18,7 @@ class NodeGraphComponent:
 	def __init__(self,mainWindow) -> None:
 		graph = NodeGraph()
 		self.graph = graph
+		self.mainWindow = mainWindow
 		
 		dock = QDockWidget("Editor main")
 		dock.setWidget(graph.widget)
@@ -42,6 +44,7 @@ class NodeGraphComponent:
 		graph.show()
 		self._addEvents()
 		self.registerNodes()
+		self.contextMenuLoad()
 
 		node : RuntimeNode = graph.create_node('runtime_domain.RuntimeNode')
 		node.add_input('in', color=(0, 80, 0))
@@ -79,17 +82,17 @@ class NodeGraphComponent:
 		#properties_bin.setWindowFlags(QtCore.Qt.Tool)
 		self.update(node)
 		
-		groupnode : GroupNode = graph.create_node("runtime_domain.RuntimeGroup")
+		"""groupnode : GroupNode = graph.create_node("runtime_domain.RuntimeGroup")
 		groupnode.set_name("TESTNAME <b>TEST</b>")
 		groupnode.add_input("input")
 		groupnode.add_output("ouput")
 		groupnode.expand()
-		sg = graph.sub_graphs
+		sg = graph.sub_graphs"""
 
 		graph.auto_layout_nodes()
 
-		n_backdrop = graph.create_node('Backdrop')
-		n_backdrop.wrap_nodes([groupnode, node])
+		#n_backdrop = graph.create_node('Backdrop')
+		#n_backdrop.wrap_nodes([groupnode, node])
 	
 	def getGraphSystem(self) -> NodeGraph:
 		return self.graph
@@ -136,6 +139,79 @@ class NodeGraphComponent:
 			node.expand()
 		pass
 	
+	def contextMenuLoad(self):
+		gmenu : NodeGraphMenu = self.graph.get_context_menu("graph")
+		nmenu : NodesMenu = self.graph.get_context_menu("nodes")
+		
+		def test_func(graph, node):
+			print('Clicked on node: {}'.format(node.name()))
+		#sect=nmenu.add_menu("TEST NODE MENU")
+		#sect.add_command("testcmd",node_class=RuntimeNode,func=test_func)
+
+		def my_test(graph):
+			"""ps = graph.viewer().scene_cursor_pos()
+			node : RuntimeNode = graph.create_node('runtime_domain.RuntimeNode', pos=[ps.x(),ps.y()])
+			node.add_input('in', color=(0, 80, 0))
+			node.add_output('out',False,False)
+			node.add_text_input('text1',"testlable",'default',"displaytab")"""
+
+			"""node.add_text_input('text2',"testlable")
+			node.add_text_input('text3',"testlable")
+			node.add_checkbox("cb",text="this is value a testing data")
+			node.add_combo_menu("cm","combo",["фыфыфыфы","ЙЙЙЙ ","СЕСЕСЕС"])
+			node.set_icon("data\\function_sim.png")
+			node.set_property("name","<b>Действие</b><br/><font size=""4""><i>Дополнительные данные</i></font>",False)
+			node.update()"""
+
+			"""groupnode : GroupNode = graph.create_node("runtime_domain.RuntimeGroup")
+			groupnode.add_input("input")
+			groupnode.add_output("ouput")"""
+		
+		gmenu.add_command('Create testobj', my_test, 'Shift+t')
+		def sertest__(graph):
+			graph.save_session(".\\session.json")
+			print("SERIALIZED")
+		gmenu.add_command("serializetest",sertest__)
+		def desertest__(graph):
+			graph.load_session(".\\session.json")
+			print("LOADED")
+		gmenu.add_command("DESER",desertest__)
+		def tsc__(graph: NodeGraph):
+			#pos = QtGui.QCursor.pos()
+			#print([(w.underMouse(),w) for w in self.widgets_at(pos)])
+			graph.toggle_node_search(True)
+		gmenu.add_command("TOGGLESEARCH",tsc__,"Tab")
+		gmenu.add_separator()
+		gmenu.add_menu("Файл")
+		gmenu.add_menu("Сцена")
+
+		pass
+
 	def onMouseClicked(self,posx,posy):
 		print(f">>>>>>>>{self} {posx} {posy}")
 		pass
+
+	#debug func
+	def widgets_at(self,pos):
+		"""Return ALL widgets at `pos`
+
+		Arguments:
+			pos (QPoint): Position at which to get widgets
+
+		"""
+
+		widgets = []
+		widget_at = QApplication.instance().widgetAt(pos)
+
+		while widget_at:
+			widgets.append(widget_at)
+
+			# Make widget invisible to further enquiries
+			widget_at.setAttribute(QtCore.Qt.WA_TransparentForMouseEvents)
+			widget_at = QApplication.instance().widgetAt(pos)
+
+		# Restore attribute
+		for widget in widgets:
+			widget.setAttribute(QtCore.Qt.WA_TransparentForMouseEvents, False)
+
+		return widgets
