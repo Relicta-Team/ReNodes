3bc849af7cc6f5292960dac3af95ddc4c41b5859
Yodes <35444748+Yobas@users.noreply.github.com>
2023-08-30 00:11:21 +0300

    new painter: plus port

--- ReNode/ui/NodePainter.py
+++ ReNode/ui/NodePainter.py
@@ -102,6 +102,56 @@ def draw_square_port(painter, rect, info):
 
     painter.restore()
 
+def draw_plus_port(painter, rect, info):
+    """
+    Custom paint function for drawing a plus-shaped port.
+
+    Args:
+        painter (QtGui.QPainter): painter object.
+        rect (QtCore.QRectF): port rect used to describe parameters
+                              needed to draw.
+        info (dict): information describing the ports current state.
+            {
+                'port_type': 'in',
+                'color': (0, 0, 0),
+                'border_color': (255, 255, 255),
+                'multi_connection': False,
+                'connected': False,
+                'hovered': False,
+            }
+    """
+    painter.save()
+
+    # mouse over port color.
+    if info['hovered']:
+        color = QtGui.QColor(14, 45, 59)
+        border_color = QtGui.QColor(136, 255, 35, 255)
+    # port connected color.
+    elif info['connected']:
+        color = QtGui.QColor(195, 60, 60)
+        border_color = QtGui.QColor(200, 130, 70)
+    # default port color
+    else:
+        color = QtGui.QColor(*info['color'])
+        border_color = QtGui.QColor(*info['border_color'])
+
+    pen = QtGui.QPen(border_color, 1.8)
+    pen.setJoinStyle(QtCore.Qt.MiterJoin)
+
+    # Draw plus symbol
+    painter.setPen(pen)
+    painter.setBrush(color)
+
+    # Draw vertical line
+    v_line_rect = QtCore.QRectF(rect.center().x() - 1, rect.top(), 2, rect.height())
+    painter.drawRect(v_line_rect)
+
+    # Draw horizontal line
+    h_line_rect = QtCore.QRectF(rect.left(), rect.center().y() - 1, rect.width(), 2)
+    painter.drawRect(h_line_rect)
+
+    painter.restore()
+
 # TODO; pins for all types, float,int,bool etc
 
 """class CustomPortsNode(BaseNode):
