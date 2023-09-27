78cd51893f6e4c7ead14217fefd4654934138c24
Yodes <35444748+Yobas@users.noreply.github.com>
2023-09-27 14:38:25 +0300

    Impl from source 375

--- ReNode/ui/NodeGraphComponent.py
+++ ReNode/ui/NodeGraphComponent.py
@@ -173,10 +173,10 @@ class NodeGraphComponent:
 		nmenu.add_command("testcmd",func=test_func,node_type='all') #,node_type='operators.if_branch'
 		
 		
-		nmenu.add_command("Скопировать",func=copy_nodes,node_type="all")
-		nmenu.add_command("Вырезать",func=cut_nodes,node_type="all")
-		nmenu.add_command("Вставить",func=paste_nodes,node_type="all")
-		nmenu.add_command("Удалить выделенные",func=delete_nodes,node_type="all")
+		nmenu.add_command("Скопировать",func=copy_nodes,node_type="all",shortcut="Ctrl+C")
+		nmenu.add_command("Вырезать",func=cut_nodes,node_type="all",shortcut="Ctrl+X")
+		nmenu.add_command("Вставить",func=paste_nodes,node_type="all",shortcut="Ctrl+V")
+		nmenu.add_command("Удалить выделенные",func=delete_nodes,node_type="all",shortcut="Del")
 		
 
 		def my_test(graph):
