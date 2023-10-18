af416ed3a4e427b8fabb334ea7e6a92407b019f8
Yodes <35444748+Yobas@users.noreply.github.com>
2023-10-18 21:28:40 +0300

    varmanager update

--- ReNode/ui/VariableManager.py
+++ ReNode/ui/VariableManager.py
@@ -1,4 +1,20 @@
 from PyQt5.QtWidgets import QMainWindow,QLabel, QDockWidget, QWidget, QVBoxLayout, QComboBox, QLineEdit, QPushButton, QTreeWidget, QTreeWidgetItem
+from PyQt5.QtCore import *
+
+from NodeGraphQt.custom_widgets.properties_bin.custom_widget_slider import *
+from NodeGraphQt.custom_widgets.properties_bin.custom_widget_value_edit import *
+from NodeGraphQt.custom_widgets.properties_bin.prop_widgets_base import *
+
+
+class VariableInfo:
+    def __init__(self):
+        pass
+
+class VariableTypedef:
+    def __init__(self,vart="",vartText="",classMaker=None):
+        self.variableType = vart
+        self.variableTypeName = vartText
+        self.classInstance = classMaker
 
 class VariableManager(QDockWidget):
     def __init__(self,actionVarViewer = None):
@@ -11,6 +27,15 @@ class VariableManager(QDockWidget):
 
         self.actionVarViewer = actionVarViewer
         
+        self.localVariables = {}
+        self.classVariables = {}
+        
+        self.variableTempateData = [
+            VariableTypedef("int","Целое число",IntValueEdit),
+            VariableTypedef("float","Дробное число",FloatValueEdit),
+            VariableTypedef("string","Строка",PropTextEdit)
+        ]
+
         self.initUI()
         
     def syncActionText(self,initState=None):
@@ -28,29 +53,39 @@ class VariableManager(QDockWidget):
         # Создайте вертикальный макет для центрального виджета
         layout = QVBoxLayout()
 
-        layout.addWidget(QLabel("Variable Type"))
+        layout.addWidget(QLabel("Тип переменной:"))
         # Выпадающий список для выбора типа переменной
         variable_type_combo = QComboBox()
-        variable_type_combo.addItems(["int", "float", "string", "bool"])
+        for vobj in self.variableTempateData:
+            variable_type_combo.addItem(vobj.variableTypeName,vobj)
+        variable_type_combo.currentIndexChanged.connect(self._onVariableTypeChanged)
         layout.addWidget(variable_type_combo)
         self.variable_type_combo = variable_type_combo
 
-        layout.addWidget(QLabel("Variable Name"))
+        layout.addWidget(QLabel("Имя переменной:"))
         # Поле для ввода имени переменной
         variable_name_input = QLineEdit()
         layout.addWidget(variable_name_input)
         self.variable_name_input = variable_name_input
 
-        layout.addWidget(QLabel("Default Value"))
+        layout.addWidget(QLabel("Начальное значение:"))
         # Поле для ввода дефолтного значения
         default_value_input = QLineEdit()
         layout.addWidget(default_value_input)
         self.default_value_input = default_value_input
 
+        self.tempvalue = None
+        prop = PropSlider()
+        layout.addWidget(prop)
+        self.tempvalue = prop
+        self.lay = layout
+
         # Кнопка создания переменной
         create_variable_button = QPushButton("Create Variable")
+        create_variable_button.setMinimumWidth(200)
+        create_variable_button.setMinimumHeight(40)
         create_variable_button.clicked.connect(self.createVariable)
-        layout.addWidget(create_variable_button)
+        layout.addWidget(create_variable_button,alignment=Qt.AlignmentFlag.AlignCenter)
 
         # Дерево для отображения списка переменных
         variable_tree = QTreeWidget()
@@ -59,6 +94,14 @@ class VariableManager(QDockWidget):
         self.variable_tree = variable_tree
 
         central_widget.setLayout(layout)
+
+    def _onVariableTypeChanged(self, *args, **kwargs):
+        
+        self.tempvalue.deleteLater()
+        layout = self.lay
+        self.tempvalue = PropTextEdit()
+        layout.insertWidget(4,self.tempvalue)
+        pass
 
     def createVariable(self):
         # Получите значения типа переменной, имени и дефолтного значения
