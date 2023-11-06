db0f03235aa7dea43249fed68ad7bd699adc74c1
Yodes <35444748+Yobas@users.noreply.github.com>
2023-11-06 13:38:45 +0300

    Command system, minor fixes

--- ReNode/ui/LoggerConsole.py
+++ ReNode/ui/LoggerConsole.py
@@ -4,6 +4,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import QIcon, QPixmap,QColor, QPainter
 import logging
 import re
+from ReNode.app.Logger import RegisterLogger
+from PyQt5.sip import *
 
 class ClickableTextBrowser(QTextBrowser):
     clicked = pyqtSignal(str)
@@ -30,6 +32,8 @@ class LoggerConsole(QDockWidget):
     def __init__(self):
         LoggerConsole.refObject = self
         super().__init__("Консоль")
+
+        self.logger : logging.Logger = RegisterLogger("Console")
 
         self.log_text = ClickableTextBrowser() #QTextEdit()
         self.log_text.setReadOnly(True)
@@ -60,18 +64,18 @@ class LoggerConsole(QDockWidget):
         
         text_browser.setStyleSheet('''
             QTextBrowser {
-                font-family: "Consolas"; /* Шрифт, похожий на тот, что используется в терминале VS Code */
-                font-size: 14px; /* Размер шрифта */
-                padding: 1px; /* Отступы вокруг текста */
+                font-family: "Consolas";
+                font-size: 14px;
+                padding: 1px;
             }
 
             a {
-                text-decoration: underline; /* Подчеркивание ссылок */
-                color: yellow; /* Цвет ссылок (по умолчанию в VS Code) */
+                text-decoration: underline;
+                color: yellow;
             }
 
             a:hover {
-                color: yellow; /* Цвет ссылки при наведении (по умолчанию в VS Code) */
+                color: yellow;
             }
                                    
             
@@ -103,6 +107,8 @@ class LoggerConsole(QDockWidget):
         if initState:
             condition = initState
         newtext = "&Скрыть консоль" if condition else "&Показать консоль"
+        if not condition:
+            self.command_input.clearFocus()
         loggerAct.setText(newtext)
 
     def addLog(self, text,levelname="INFO",logname=""):
@@ -125,10 +131,13 @@ class LoggerConsole(QDockWidget):
         #fulltext = f'<pre>{fulltext}</pre>'
         
         if not self.log_text: return
+        if isdeleted(self.log_text): return
         
         self.log_text.setHtml(fulltext)
 
         self.log_text.verticalScrollBar().setValue(self.log_text.verticalScrollBar().maximum())
+
+            
 
     def wrapNodeLink(nodeid,text=None,color='#17E62C'):
         if not text: text = nodeid
@@ -151,4 +160,148 @@ class LoggerConsole(QDockWidget):
         self.setWidget(widget)
 
         #! now input in debug not enabled
-        log_layout.removeItem(input_layout)
+        #log_layout.removeItem(input_layout)
+
+        self.command_input.returnPressed.connect(self.execute_command)
+        self.command_input.editingFinished.connect(lambda: self.showStatusTipMessage())
+
+        self.command_completer = QCompleter(self)
+        self.command_completer.setFilterMode(Qt.MatchContains)
+        self.command_input.setCompleter(self.command_completer)
+        def __onselectcmd(text):
+            cmd = self._findCmdByName(text)
+            if cmd:
+                self.showStatusTipMessage(cmd.desc)
+        
+        #self.command_completer.activated.connect(lambda text: self.showStatusTipMessage(""))
+        self.command_completer.highlighted.connect(__onselectcmd)
+        
+        self._commands = []
+        self.completer_model :QStringListModel = QStringListModel()
+        
+        self.command_completer.setModel(self.completer_model)
+        self.command_completer.setCaseSensitivity(Qt.CaseInsensitive)
+
+        self.send_button.clicked.connect(self.execute_command)
+
+        #for i in range(1,1000):
+            #cmd = ConsoleCommand(f'command {i}')
+            #self._registerCommand(cmd)
+        self._registerStandartCommands()
+
+    def showStatusTipMessage(self,mes=""):
+        from ReNode.ui.NodeGraphComponent import NodeGraphComponent
+        NodeGraphComponent.refObject.mainWindow.statusBar().showMessage(mes)
+
+    def execute_command(self):
+        if self.command_completer.popup().isVisible(): 
+            self.command_completer.popup().setVisible(False)
+            #self.showStatusTipMessage("")
+            return
+        
+        # Получаем введенную команду
+        command = self.command_input.text()
+        
+        # Очищаем поле ввода команды
+        self.command_input.clear()
+        
+        cmdList = command.split(' ')
+        if len(cmdList) == 0:
+            return
+        command = cmdList[0]
+        args = []
+        if len(cmdList) > 1:
+            args = cmdList[1:]
+
+        cmd = self._findCmdByName(command)
+        if not cmd:
+            self.logger.error(f"<span style=\"color:yellow\">Unknown command: {command}</span>")
+            return
+        # Выполняем команду (замените этот код на реальную логику выполнения команд)
+        self.logger.debug(f"Executing command: {command} with args {args}")
+        cmd.onCall(args)
+
+    def _registerCommand(self, command_type):
+        command = command_type() if isinstance(command_type,type) else command_type
+        self._commands.append(command)
+
+        cmdlist = [f"{cmd.name}" for cmd in self._commands]
+        self.completer_model.setStringList(cmdlist)
+    
+    def _findCmdByInfo(self,info):
+        for cmd in self._commands:
+            if cmd.getCmdInfo() == info:
+                return cmd.name
+        return ""
+    
+    def _findCmdByName(self,name):
+        for cmd in self._commands:
+            if cmd.name == name:
+                return cmd
+        return None
+
+    def _registerStandartCommands(self):
+        self._registerCommand(HelpCommand)
+        self._registerCommand(SessionClipSaveCommand)
+        self._registerCommand(SessionClipLoadCommand)
+
+class ConsoleCommand:
+    name = "default command"
+    desc = "no description"
+    def __init__(self,name=None,desc=None) -> None:
+        if not name:
+            name = self.__class__.name
+        if not desc:
+            desc = self.__class__.desc
+        self.name = name
+        self.desc = desc
+
+        self.logger = LoggerConsole.refObject.logger
+    
+    def getCmdInfo(self):
+        return f'{self.name} - {self.desc}'
+
+    def getLoggerInstance(self) -> LoggerConsole:
+        return LoggerConsole.refObject
+
+    def onCall(self,args):
+        pass
+
+class HelpCommand(ConsoleCommand):
+    name = "help"
+    desc = "Выводит описание всех доступных команд"
+
+    def onCall(self,args):
+        cmdData = "\tДоступные комадны:"
+        for cmd in self.getLoggerInstance()._commands:
+            cmdData += f'\n\t{cmd.name} - {cmd.desc}'
+        self.logger.info(f'<span style="color:lightblue">{cmdData}</span>')
+
+
+class SessionClipSaveCommand(ConsoleCommand):
+    name = "clip_save"
+    desc = "Сохраняет сессию в буфер обмена"
+    def onCall(self,args):
+        from ReNode.ui.NodeGraphComponent import NodeGraphComponent
+        if NodeGraphComponent.refObject:
+            graph = NodeGraphComponent.refObject.graph
+            nodes = graph.all_nodes()
+            data = graph.serializedGraphToString(graph._serialize(nodes,serializeMouse=True))
+            if data:
+                from PyQt5.QtWidgets import QApplication
+                QApplication.clipboard().setText(data)
+                self.logger.info("Данные сохранены")
+
+class SessionClipLoadCommand(ConsoleCommand):
+    name = "clip_load"
+    desc = "Загружает сессию из буфера обмена"
+    def onCall(self,args):
+        from ReNode.ui.NodeGraphComponent import NodeGraphComponent
+        from PyQt5.QtWidgets import QApplication
+
+        if NodeGraphComponent.refObject:
+            graph = NodeGraphComponent.refObject.graph
+            data = QApplication.clipboard().text()
+            if data:
+                graph.loadGraphFromString(data,loadMousePos=True)
+                self.logger.info("Данные загружены")
