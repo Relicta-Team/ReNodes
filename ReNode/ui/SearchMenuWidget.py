e0156539fe1ce913d18b697068e8112a5bcb72d7
Yodes <35444748+Yobas@users.noreply.github.com>
2023-11-11 21:27:01 +0300

    wip #29

---
+++ ReNode/ui/SearchMenuWidget.py
@@ -0,0 +1,9 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class SeachMenuWidget(QWidget):
+    pass
+
+class SeachMenuLineEditWidget(QtWidgets.QLineEdit):
+    pass
