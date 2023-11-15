cd7c360af27f80ecdf37ea14b1679f209d5ecab0
Yodes <35444748+Yobas@users.noreply.github.com>
2023-11-15 21:28:27 +0300

    lib generator update (wip)

--- ReNode/app/LibGenerator.py
+++ ReNode/app/LibGenerator.py
@@ -2,6 +2,7 @@
 import re
 import os
 import json
+import copy
 from ReNode.app.utils import intTryParse
 
 def checkRegionName(content,regname : str):
@@ -43,32 +44,113 @@ class NodeObjectHandler:
 		self.objectNameFull = objName #базовое полное имя члена
 		self.objectType = objectType #тип-категория члена (метод, поле, функция)
 
-		self.lineList = __lineList #ссылка на список обрабатываемых строк
+		self.lineList : list = __lineList #ссылка на список обрабатываемых строк
 
 		self.isField = self.objectType == 'f'
 		self.isMethod = self.objectType == 'm'
 		self.isSystem = self.objectType == 'node'
 
+		self.refAllObjects : list = None #Ссылка на все объекты
+		self.counterCopy = 0
+
 		self.memberData = {
 			"inputs": {},
-			"outputs": {}
+			"outputs": {},
+			"options": {}
 		}
 		self.lastPortRef = None
+
+		# Определение имени класса и имени члена
+		self.memberClass = None
+		self.memberName = None
+		self.memberNameFull = None
+
+		if self.isMethod or self.isField:
+			if re.findall('_\d+$',objName): #if curMember endswith regex _\d+ then remove postfix
+				clearMem = re.sub(r'_\d+$',"",objName)#curMember[:curMember.rfind('_')]
+			else:
+				clearMem = objName
+			className_, memName_ = clearMem.lower().split('.',1)
+			self.memberClass = className_
+			self.memberName = memName_
+			self.memberNameFull = f'{className_}.{memName_}'
 
 	def __repr__(self) -> str:
 		return f'Node:{self.objectNameFull} at {hex(id(self))}'
 
+	def exThrow(self,msg='Unhandled exception'):
+		raise Exception(f'[{self.objectNameFull}]: {msg}')
+
+	def __getitem__(self, key):
+		return self.memberData[key]
+	def __setitem__(self, key, value):
+		self.memberData[key] = value
+
+	def generateJson(self):
+		for tokens in self.lineList:
+			self.handleTokens(tokens)
+		self.finalizeObject()
+		self.refAllObjects.remove(self)
+
 	#for add line once
 	def insertLine(self,string):
-		self.lineList.insert(1,string)
+		self.lineList.insert(1,getTokens(string))
 	#for add lines (example: input)
 	def insertLines(self,stringList):
-		self.lineList[1:1] = stringList
+		self.lineList[1:1] = [getTokens(o) for o in stringList]
 	
-	def copy(self):
+	def pushBackLines(self,stringList):
+		self.lineList.extend([getTokens(o) for o in stringList])
+
+	def removeLine(self,setting):
+		remtoks = None
+		for toks in self.lineList:
+			if toks[0] == setting:
+				remtoks = toks
+				break
+		if remtoks:
+			self.lineList.remove(remtoks)
+			return True
+		return False
+
+	#object copy
+	def copy(self,addToAllObjects=True):
 		src = self
-		dst = NodeObjectHandler(self.objectNameFull,self.objectType,self.nodeLib,self.classMetadata,self.lineList)
-		dst.memberData = src.memberData.copy()
+		src.counterCopy += 1
+		dst = NodeObjectHandler(self.objectNameFull,self.objectType,self.nodeLib,self.classMetadata,self.lineList.copy())
+		dst.counterCopy = src.counterCopy
+		dst.memberData = copy.deepcopy(src.memberData)
+		
+		dst.memberClass = src.memberClass
+		dst.memberName = src.memberName
+		dst.memberNameFull = src.memberNameFull
+
+		dst.refAllObjects = src.refAllObjects
+		if addToAllObjects:
+			if len(dst.refAllObjects) <= 1:
+				dst.refAllObjects.append(dst)
+			else:
+				dst.refAllObjects.insert(1,dst)
+		return dst
+
+	def preparePath(self,path):
+		return path.replace('\\\\','\\').replace('\\','\\\\')
+
+	def prepareCode(self,code):
+		if self.isMethod:
+			code = code.replace('@thisMethod',self.memberName)
+			if "@thisParams" in code:
+				
+				paramList = ['\'this\'']
+				ctr = 0
+				for indexVar, (outKey,outValues) in enumerate(self['outputs'].items()):
+					if outValues['type'] != "Exec" and outValues['type'] != "":
+						paramList.append('\"@genvar.out.{}\"'.format(indexVar+1))
+				code = code.replace('@thisClass',self.memberClass)
+				paramCtx = f'params [{", ".join(paramList)}]'
+				code = code.replace('@thisParams',paramCtx)
+		
+		return code
 
 	def handleTokens(self,tokens):
 		tokenType = tokens[0]
@@ -98,7 +180,6 @@ class NodeObjectHandler:
 		elif tokenType == 'classprop':
 			clsprop = intTryParse(tokens[1])
 			if clsprop:
-				#todo need return type
 				self.memberData['classProp'] = clsprop
 			pass
 		#method specific data
@@ -106,10 +187,8 @@ class NodeObjectHandler:
 		#method type: method,event,get,const
 		elif tokenType == 'type':
 			mtype = tokens[1]
-			if mtype not in ['method','event','get','const']:
-				raise ValueError(f"[{self.objectNameFull}]: Wrong method type: {mtype}")
 			self.memberData['type'] = mtype
-			#todo define icon and node color by type
+
 			#const - add classProp
 			if mtype == "const":
 				self.memberData['classProp'] = 1
@@ -160,96 +239,183 @@ class NodeObjectHandler:
 					self.lastPortRef['display_name'] = intTryParse(tInside.split('=')[1]) > 0
 				if tInside.startswith('allowtypes'):
 					self.lastPortRef['allowtypes'] = tInside.split('|')
+		
+		# -------------------- common spec options -------------------- 
+		
+		# redef node name
+		elif tokenType == 'node':
+			self.objectNameFull = tokens[1]
+		# def visible node in treeview
+		elif tokenType == "libvisible":
+			if intTryParse(tokens[1]) == 0:
+				self.memberData['isVisibleInLib'] = False
+		# enable ports with autotypes
+		elif tokenType == "runtimeports":
+			if intTryParse(tokens[1]) > 0:
+				self.memberData['runtime_ports'] = True
+		# add specific option to node
+		elif tokenType == "option":
+			_cont :str= tokens[1]
+			if not _cont.strip(' \t\n\r').startswith("{"):
+				_cont = "{"+_cont+"}"
+			_ser = json.loads(_cont)
+			for k,v in _ser.items():
+				if k in self['options']:
+					self.exThrow(f"Duplicate option: {k}")
+				self['options'][k] = v
+
 		pass
 	def finalizeObject(self):
-		curMember = self.objectNameFull
-		curMemberType = self.objectType
 		classmeta = self.classMetadata
 		memberData = self.memberData
-		if re.match(r'_\d+$',curMember):
-			clearMem = curMember[:curMember.rfind('_')]
-		else:
-			clearMem = curMember
-		className_, memName_ = clearMem.lower().split('.')
-		memberRealName = f'{className_}.{memName_}'
+
 		memberRegion = "unknown"
-		if curMemberType == 'f':
+		if self.isField:
 			memberRegion = "fields"
-		elif curMemberType == 'm':
+		elif self.isMethod:
 			memberRegion = "methods"
-		elif curMemberType == 'node':
-			memberRegion = "nodes"
+		elif self.isSystem:
+			memberRegion = self.objectType #internal,operators,etc
 		else:
-			raise Exception(f'Unknown member type: {curMemberType}')
+			raise Exception(f'Unknown member type: {self.objectType}')
 
 		if memberRegion not in self.nodeLib:
 			self.nodeLib[memberRegion] = {}
 
 		# Проверка возвращаемого типа и его автоопределение
 		if 'returnType' not in memberData:
-			#if curMember endswith regex _\d+ then remove postfix
 			
-			if curMemberType == 'f':
-				memberData['returnType'] = classmeta[className_]['fields']['defined'].get(memName_,'NULLTYPE_ALLOC')
+			if self.isField:
+				memberData['returnType'] = classmeta[self.memberClass]['fields']['defined'].get(self.memberName,'NULLTYPE_ALLOC')
 			else:
 				memberData['returnType'] = "return_void"
 
+		# Проверка типа и его автоопределение
+		if 'type' not in memberData:
+			if self.isMethod:
+				memberData['type'] = "method"
+
+		#validate color, icon, code
+		if self.isMethod:
+			mtype = self['type']
+
+			#! fix type for nodes
+			self['memtype'] = mtype
+			del memberData['type']
+
+			tableTypes = ["method","event","get","const"]
+			if mtype not in tableTypes:
+				raise ValueError(f"[{self.objectNameFull}]: Wrong method type: {mtype}")
+
+			_canOverrideCode = "code" not in self.memberData
+			_canOverrideColor = "color" not in self.memberData
+			_canOverrideIcon = 'icon' not in self.memberData
+			if _canOverrideCode:
+				if mtype == "get":
+					newcode = 'func(@thisMethod) {@thisParams; @out.1}'
+				elif mtype == "const":
+					newcode = 'func(@thisMethod) { @propvalue }'
+				else:
+					newcode = 'func(@thisMethod) {@thisParams; @out.1}'
+				self['code'] = newcode
+			if _canOverrideColor:
+				nodeColorList = [
+					[255,255,255],#method
+					[255,255,255],#event
+					[255,255,255],#getter
+					[255,255,255],#constant
+				]
+				newColor = nodeColorList[tableTypes.index(mtype)]
+				self['color'] = newColor
+			if _canOverrideIcon:
+				iconList = [
+					"test.png",#method
+					"test.png",#event
+					"test.png",#getter
+					"test.png",#constant
+				]
+				self['icon'] = iconList[tableTypes.index(mtype)]
+
+
+		if "classProp" in memberData:
+			if 'returnType' not in memberData:
+				raise Exception(f'[{self.objectNameFull}]: classProp must have returnType')
+
 		# Определение свойств поля (геттеры, сеттеры)
-		if curMemberType == 'f':
+		if self.isField and self.counterCopy == 0:
 			if 'prop' not in memberData:
 				memberData['prop'] = 'all'
 			curPropType = memberData['prop']
 			del memberData['prop']
+			#removing lines for update generated
+			self.lineList.clear()
+
 			if curPropType == 'none':
 				memberData = None
 			elif curPropType in ['all','get','set']:
-				#TODO refactoring
-				orig_curMember = memberRealName
-				curMember = [orig_curMember + ".get",orig_curMember + '.set']
-				memberData = [memberData.copy(),memberData.copy()]
-				memberData[0]['code'] = f'this getVariable "{memName_}"'
-				memberData[1]['code'] = f'this setVariable ["{memName_}",@in.2]; @out.1'
-				if curPropType != "all":
-					__idx = 0 if curPropType == 'get' else 1
-					curMember = curMember[__idx]
-					memberData = memberData[__idx]
+				_hasGet = curPropType in ['all','get']
+				_hasSet = curPropType in ['all','set']
+				if _hasGet:
+					newobj = self.copy(True)
+					newobj.objectNameFull += f".get"
+					newobj['code'] = f'this getVariable "{newobj.memberName}"'
+					newobj.pushBackLines([
+						"exec:out",
+						f'out:{newobj["returnType"]}:Значение'
+					])
+				if _hasSet:
+					newobj = self.copy(True)
+					newobj.objectNameFull += f".set"
+					newobj['code'] = f'this setVariable ["{newobj.memberName}",@in.2]; @out.1'
+					newobj.pushBackLines([
+						"exec:all",
+						f"in:{newobj['returnType']}:Значение",
+						f'out:{newobj["returnType"]}:Новое значение'
+					])
+				
+				#set memdata to null 
+				memberData = None
 		
 		if memberData:
-			if isinstance(curMember,list):
-				for i,cm in enumerate(curMember):
-					self.nodeLib[cm] = memberData[i]
-			else: 
-				self.nodeLib[memberRegion][memberRealName] = memberData
+			
+			isInspectorProp = 'classProp' in memberData
+
+			#prep code and icon
+			if 'icon' in memberData: self['icon'] = self.preparePath(self['icon'])
+			if 'code' in memberData: self['code'] = self.prepareCode(self['code'])
+
+			#removing classprop
+			if isInspectorProp:
+				del memberData['classProp']
+
+				# регистрируем свойство для видимости в инспекторе
+				prps__ = classmeta[self.memberClass]
+				if not 'inspectorProps' in prps__: prps__['inspectorProps'] = []
+				prps__['inspectorProps'].append(self.objectNameFull)
+
+			# add to main lib
+			self.nodeLib[memberRegion][self.objectNameFull] = memberData
+
+			#add node inside class zone
+			if self.isField:
+				flds = classmeta[self.memberClass]['fields']
+				
+				if 'nodes' not in flds: flds['nodes'] = []
+				flds['nodes'].append(self.objectNameFull)
+
+			if self.isMethod:
+				mtds = classmeta[self.memberClass]['methods']
+				
+				if 'nodes' not in mtds: mtds['nodes'] = []
+				mtds['nodes'].append(self.objectNameFull)
 
 def compileRegion(members,nodeLib,classmeta):
 	curMember = None
 	
-	lineList = members.splitlines()
-	while len(members) > 0:
-		__line = lineList[0]
-		line = __line.lstrip('\t ')
-		if not line: continue
-		printDebug(f'compile line: {line}')
-		tokens = getTokens(line) # def,type,memname
-		if not tokens: raise ValueError(f"Wrong member line: {line}")
-		tokenType = tokens[0]
-		if tokenType == "def":
-			#new define
-			if curMember:
-				curMember.finalizeObject()
-				del curMember
-				curMember = None
-			if len(tokens) < 3: raise ValueError(f"Wrong define member line: {line}")
-			
-			memName = tokens[2]
-			memType = tokens[1]
-			curMember = NodeObjectHandler(memName,memType,nodeLib,classmeta,lineList)
-			lineList.pop(0)
-			continue
-		
-		curMember.handleTokens(tokens)
-		lineList.pop(0)
+	collectLines = []
+	objectList = []
 
+	# collecting tokens
 	for __line in members.splitlines():
 		line = __line.lstrip('\t ')
 		if not line: continue
@@ -260,20 +426,28 @@ def compileRegion(members,nodeLib,classmeta):
 		if tokenType == "def":
 			#new define
 			if curMember:
-				curMember.finalizeObject()
+				objectList.append(curMember)
+				collectLines = []
 				curMember = None
 			if len(tokens) < 3: raise ValueError(f"Wrong define member line: {line}")
 			
 			memName = tokens[2]
 			memType = tokens[1]
-			curMember = NodeObjectHandler(memName,memType,nodeLib,classmeta)
+			collectLines = []
+			curMember = NodeObjectHandler(memName,memType,nodeLib,classmeta,collectLines)
+			curMember.refAllObjects = objectList
+			continue
 		
-		curMember.handleTokens(tokens)
-		continue
-		
+		collectLines.append(tokens)
+
+	# adding last member
 	if curMember:
-		curMember.finalizeObject()
-		del curMember
+		objectList.append(curMember)
+
+	# generate libs
+	while len(objectList) > 0:
+		obj = objectList[0]
+		obj.generateJson()
 
 # generate lib (using flag -genlib)
 def GenerateLibFromObj():
