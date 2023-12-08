3451640fad5cb54c4a7c8d9676e1c18a2692782f
Yodes <35444748+Yobas@users.noreply.github.com>
2023-12-08 16:18:38 +0300

    function def wip

--- ReNode/ui/VarMgrWidgetTypes/FunctionDef.py
+++ ReNode/ui/VarMgrWidgetTypes/FunctionDef.py
@@ -2,8 +2,78 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from Qt import QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 from NodeGraphQt.custom_widgets.properties_bin.prop_widgets_base import *
 from ReNode.ui.SearchMenuWidget import SearchComboButton,addTreeContent,createTreeDataContent,addTreeContentItem
+from ReNode.app.utils import updateIconColor, mergePixmaps, generateIconParts
+
+class DataTypeSelectorGroup(QWidget):
+    def __init__(self,srcLayout=None,ofsX=0,ofsY=0):
+        super(DataTypeSelectorGroup,self).__init__()
+        from ReNode.ui.VariableManager import VariableManager
+        lay = srcLayout or QGridLayout()
+
+        contents = VariableManager.refObject.getAllTypesTreeContent()
+        comboButton = SearchComboButton()
+        comboButton.loadContents(contents)
+
+        lay.addWidget(QLabel("Тип параметра:"),ofsX+0,ofsY+0)
+        lay.addWidget(comboButton,ofsX+0,ofsY+1)
+        self.widParamType = comboButton
+
+        self.widDataType = QComboBox()
+        for vobj in VariableManager.refObject.variableDataType:
+            icn = None
+            if isinstance(vobj.icon,list):
+                for pat in vobj.icon:
+                    icntemp = QPixmap(pat)
+                    if icn:
+                        icntemp = mergePixmaps(icn,icntemp)
+                    icn = icntemp
+                icn = QIcon(icn)
+            else:
+                icn = QIcon(vobj.icon)
+            self.widDataType.addItem(icn,vobj.text)
+            self.widDataType.setItemData(self.widDataType.count()-1,vobj.dataType,Qt.UserRole)
+        self.widDataType.currentIndexChanged.connect(lambda: self.onDataTypeChanged(self.widDataType.currentIndex()))
+        lay.addWidget(QLabel("Тип данных:"),ofsX+1,ofsY+0)
+        lay.addWidget(self.widDataType,ofsX+1,ofsY+1)
+
+        self.ofsX = ofsX
+        self.ofsY = ofsY
+        self.optText = None
+        self.optData = None
+
+        optText = QLabel("Тип значений:")
+        lay.addWidget(optText,ofsX+2,ofsY+0)
+        optData = SearchComboButton()
+        optData.loadContents(contents)
+        lay.addWidget(optData,ofsX+2,ofsY+1)
+        
+        self.optData = optData
+        self.optText = optText
+
+        if not srcLayout:
+            self.setLayout(lay)
+            self.srcLayout = lay
+        else:
+            self.srcLayout = srcLayout
+        
+        self.onDataTypeChanged(0)
+
+    def updateVisual(self,tobj):
+        hasVisible = tobj.dataType == "dict"
+        self.optData.setVisible(hasVisible)
+        self.optText.setVisible(hasVisible)
+
+
+    def onDataTypeChanged(self,newIndexDatatype):
+        from ReNode.ui.VariableManager import VariableTypedef,VariableDataType,VariableManager
+        #newIndexDatatype = args[0]
+        tobj : VariableDataType = VariableManager.refObject.variableDataType[newIndexDatatype]
+        self.updateVisual(tobj)
 
 class FunctionDefWidget(QWidget):
 
@@ -38,7 +108,7 @@ class FunctionDefWidget(QWidget):
         self.scrollAreaLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
         #self.scrollAreaLayout.addStretch(1)
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.scrollArea.setMinimumHeight(200)
+        self.scrollArea.setMinimumHeight(400)
 
         self.layout.addWidget(self.scrollArea)
         self.setLayout(self.layout)
@@ -53,6 +123,8 @@ class FunctionDefWidget(QWidget):
 
         for i, elay in enumerate(self.arrayElements):
             elay.itemAt(0).widget().setText(f'{i+1}: ')
+
+        self.getParamInfo()
 
     def get_value(self):
         return None
@@ -75,6 +147,49 @@ class FunctionDefWidget(QWidget):
     def callChangeValEvent(self):
         self.value_changed.emit(self.get_value())
 
+    class InternalGrid(QGridLayout):
+        def __init__(self):
+            super().__init__()
+            self.paramName = None
+            self.paramDesc = None
+            self.paramType = None
+            self.paramDataType = None
+            self.paramValueTypes = None #for dict
+
+    def getParamInfo(self):
+        """Получает массив метаданных для создания функции
+        
+            {
+                "name": paramName,
+                "desc": paramDesc,
+                "dataType": paramDataType,
+                "type": paramType,
+                "optValType": optValType
+            }
+        
+        """
+        
+        paramData = []
+        for itm in self.arrayElements:
+            grid : FunctionDefWidget.InternalGrid = itm.itemAt(1).widget().layout()
+            paramName = grid.paramName.text().rstrip(' ').lstrip(' ')
+            paramDesc = grid.paramDesc.text().rstrip(' ').lstrip(' ')
+            paramDataType = grid.paramDataType.currentData() #value,dict,array,etc...
+            paramType = grid.paramType.get_value()
+            optValType = grid.paramValueTypes.get_value() #for dict (value type)
+
+            print(f"{paramDataType}[{paramType},{optValType}] {paramName} - {paramDesc}")
+            paramData.append({
+                "name": paramName,
+                "desc": paramDesc,
+                "dataType": paramDataType,
+                "type": paramType,
+                "optValType": optValType
+            })
+
+        return paramData
+
+
     def addArrayElement(self,val=None):
         from ReNode.ui.VariableManager import VariableManager
 
@@ -89,33 +204,49 @@ class FunctionDefWidget(QWidget):
 
         # Создайте виджет для значения элемента (может быть QLineEdit, QSpinBox или другой)
         elementValueWidget = None
-        
-        grid = QGridLayout()
-        elementLayout.addLayout(grid)
+
+        grid = FunctionDefWidget.InternalGrid()
+        mainGroup = QWidget()
+        mainGroup.setLayout(grid)
+        elementLayout.addWidget(mainGroup)
+        #elementLayout.addLayout(grid)
 
         paramName = QLineEdit()
+        optWid = paramName.fontMetrics().width("Имя параметра")
+        paramName.setMaximumWidth(optWid + 10)
         paramName.setPlaceholderText("Имя параметра")
         paramDesc = QLineEdit()
         paramDesc.setPlaceholderText("Описание параметра (опционально)")
-        grid.addWidget(paramName,0,0,alignment=Qt.AlignmentFlag.AlignLeft)
+        grid.addWidget(paramName,0,0)
         grid.addWidget(paramDesc,0,1)
+        
+        groupType = DataTypeSelectorGroup(srcLayout=grid,ofsX=2,ofsY=0)
+        
+        #init references
+        grid.paramName = paramName
+        grid.paramDesc = paramDesc
+        grid.paramType = groupType.widParamType
+        grid.paramDataType = groupType.widDataType
+        grid.paramValueTypes = groupType.optData
 
-        paramTypeDesc = QLabel('Тип параметра:')
-        paramTypeDesc.setToolTip("Тип данных параметра")
-        contents = VariableManager.refObject.getAllTypesTreeContent()
-        comboButton = SearchComboButton()
-        comboButton.loadContents(contents)
-        grid.addWidget(paramTypeDesc,1,0,alignment=Qt.AlignmentFlag.AlignLeft)
-        grid.addWidget(comboButton,1,1)
+        #grid.addWidget(groupType,1,0)
 
-        paramDataTypeDesc = QLabel("Тип данных:")
-        paramDataTypeDesc.setToolTip("Тип данных параметра")
-        grid.addWidget(paramDataTypeDesc,2,0,alignment=Qt.AlignmentFlag.AlignLeft)
-        paramDataType = QComboBox()
-        grid.addWidget(paramDataType,2,1)
-        paramDataTypeSelect = SearchComboButton()
-        paramDataTypeSelect.loadContents(contents)
-        grid.addWidget(paramDataTypeSelect,2,2)
+        # paramTypeDesc = QLabel('Тип параметра:')
+        # paramTypeDesc.setToolTip("Тип данных параметра")
+        # contents = VariableManager.refObject.getAllTypesTreeContent()
+        # comboButton = SearchComboButton()
+        # comboButton.loadContents(contents)
+        # grid.addWidget(paramTypeDesc,1,0,alignment=Qt.AlignmentFlag.AlignLeft)
+        # grid.addWidget(comboButton,1,1)
+
+        # paramDataTypeDesc = QLabel("Тип данных:")
+        # paramDataTypeDesc.setToolTip("Тип данных параметра")
+        # grid.addWidget(paramDataTypeDesc,2,0,alignment=Qt.AlignmentFlag.AlignLeft)
+        # paramDataType = QComboBox()
+        # grid.addWidget(paramDataType,2,1)
+        # paramDataTypeSelect = SearchComboButton()
+        # paramDataTypeSelect.loadContents(contents)
+        # grid.addWidget(paramDataTypeSelect,2,2)
         
 
         #elementLayout.addWidget(elementValueWidget)
