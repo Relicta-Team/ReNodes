b1bd9841a516a6029d86428d4e67f92c77389e8f
Yodes <35444748+Yobas@users.noreply.github.com>
2024-01-21 16:15:13 +0300

    fixed string widgets in node

--- NodeGraphQt/widgets/node_widgets.py
+++ NodeGraphQt/widgets/node_widgets.py
@@ -576,43 +576,16 @@ class NodeTextEdit(NodeBaseWidget):
 
         widget = self
         def _redr():
-            #ledit.update()
-            #ledit.updateGeometry()
-            #widget.widget().setFixedSize(widget.get_custom_widget().sizeHint())
-            #widget.resize(QtCore.QSizeF(widget.get_custom_widget().sizeHint()))
-            curSize = self.get_custom_widget().rect() 
+
             maxSize = self.get_custom_widget().sizeHint()
-            size = self.get_custom_widget().size()
-            v3 = (maxSize,curSize,size)
-            newsize = QtCore.QSize(maxSize.width(),max(maxSize.height(),curSize.bottom()))
+            #curSize = self.get_custom_widget().rect() 
+            #size = self.get_custom_widget().size()
+            #v3 = (maxSize,curSize,size)
+            #newsize = QtCore.QSize(maxSize.width(),max(maxSize.height(),curSize.bottom()))
             self.get_custom_widget().setFixedSize(maxSize)
-            self.widget().resize(maxSize)
-
-            ledit.blockSignals(True)
-            ledit.updateGeometry()
-            self.updateGeometry()
-            self.widget().updateGeometry()
-            ledit.blockSignals(False)
+            self.widget().setFixedHeight(maxSize.height() + 17) #! 17 - HARD CONSTANT....
             
-            #self.update()
-            #self.node.draw_node()
-            #self.widget().update()
-
-            #self.widget().updateGeometry()
-            #self.updateGeometry()
-            #widget.get_custom_widget().updateGeometry()
-            
-            #self.view.update()
-            #self.view.draw_node()
-        #self.get_custom_widget().textChanged.connect(lambda: _redr())
-        #_redr()
         ledit.on_geometry_updated.connect(_redr)
-        
-        # ledit.updateGeometry()
-        # self.widget().resize(self.get_custom_widget().sizeHint())
-        # self.get_custom_widget().setFixedSize(self.get_custom_widget().sizeHint())
-        # self.update()
-        # self.node.draw_node()
 
     def boundingRect(self) -> QtCore.QRectF:
         baseRect = super().boundingRect()
@@ -620,7 +593,7 @@ class NodeTextEdit(NodeBaseWidget):
         size = self.get_custom_widget()
         if size:
             geoh = size.geometry().y()
-            curh = size.sizeHint().height()#max(curh,size.sizeHint().height())
+            curh = size.sizeHint().height()
             curh += geoh
         
         return QtCore.QRectF(baseRect.x(),baseRect.y(),baseRect.width(),curh)
